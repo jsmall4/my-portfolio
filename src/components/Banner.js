07b2164e2b15a87d733cc7975d90d5b13208bacb
jsmall4 <64646369+jsmall4@users.noreply.github.com>
2022-11-20 12:47:42 +0000

    started animation for header image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,24 @@
+import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import headerImage from "../assets/img/loading.gif";
 
 export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Development", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const [delta, setDelta] useState(300 - Math.random() * 100)
+  const period = 2000;
+
+  useEffect(() =>{
+    let ticker = setInterval (() => {
+      ticker();
+
+    }, delta)
+    return () => {clearInterval(ticker)}
+
+  }, [text])
+  
   return (
     <section className="banner" id="home">
       <Container>
@@ -9,7 +26,7 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="Hi, I'm Jack"></span>
             <h1>
-              {`Welcome to my Portfolio`}
+              {`Welcome to my Portfolio`} <br></br>
               <span className="wrap">Learning all things Web Development!</span>
             </h1>
             <p>

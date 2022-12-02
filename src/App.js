7a47d9c93046ca9460d7c5331fb72987174d7ea5
jsmall4 <64646369+jsmall4@users.noreply.github.com>
2022-12-02 15:26:34 +0000

    added projects but not finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Projects } from "./components/Projects";
+import { ProjectCard } from "./components/ProjectCard";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <ProjectCard />
+      <Projects />
     </div>
   );
 }
